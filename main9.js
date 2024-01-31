f62c282869f1b87abcc8bf50602c6c7643b7eed5
DariaS30 <daryasas010@gmail.com>
2024-01-31 20:26:15 +0200

    hw-9 was done

---
+++ main9.js
@@ -0,0 +1,12 @@
+
+function removeElement(array, item) {
+    const index = array.indexOf(item);
+
+    if (index !== -1) {
+        array.splice(index, 1);
+    }
+}
+
+const array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 7);
+console.log(array);
